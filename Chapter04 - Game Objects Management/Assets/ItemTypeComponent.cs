2426b1f3cbdcd8f9f658682d6d1ec5d8b02362f3
DESKTOP-325D3AA\Slowanolf <pocawin_soiyana@cmu.ac.th>
2020-08-14 16:36:09 +0700

    Lab 4

---
+++ Chapter04 - Game Objects Management/Assets/ItemTypeComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTypeComponent : MonoBehaviour
+{
+    [SerializeField]
+     protected ItemType _itemType;
+     public ItemType Type
+     {
+     get
+     {
+     return _itemType;
+     }
+     set
+     {
+     _itemType = value;
+    
+     }
+     }
+}
